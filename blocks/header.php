f2932b609273e3a6322880fb6c378d7178435efa
Shenojee <158710492+Shenojee@users.noreply.github.com>
2024-02-03 12:19:20 +0300

    Create header.php

---
+++ blocks/header.php
@@ -0,0 +1,30 @@
+<!-- Шапка -->
+<header data-bs-theme="white">
+ <nav class="navbar navbar-expand-md navbar-white fixed-top bg-white">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <div>
+        <a href="ссылка_на_главную"> <img src="img/logo.png" width="100px"</img></a>
+      </div>
+        <a class="navbar-brand fst-italic" href="#" style="font-family:trebuchet ms; color:black; font-size:40px">My-Giro</a>
+        <button class="navbar-toggler" type="button"
+                data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+                aria-controls="navbarCollapse" aria-expanded="false" aria-label="Переключение навигации">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#" style="font-family:Century gothic; font-size:20px">Телефон:</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" style="font-family:Century gothic; font-size:20px; color:black">8 (800) 555-35-35</a>
+          </li>
+        </ul>
+      <form class="d-flex" role="search">
+          <button class="btn btn-outline-success" type="submit" style="font-family:Century gothic; font-size:20px">Заказать звонок</button>
+      </form>
+    </div>
+  </div>
+ </nav>
+</header>
+<!-- Шапка END -->
