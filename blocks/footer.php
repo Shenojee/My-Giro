64fad9fb90a4c3a4ba4b7c07cafcf356ca764ed2
Shenojee <158710492+Shenojee@users.noreply.github.com>
2024-02-03 12:18:23 +0300

    Create footer.php

---
+++ blocks/footer.php
@@ -0,0 +1,17 @@
+<!-- Футер -->
+<footer class="container">
+  <hr class="featurette-divider">
+    <p class="float-end"><a href="#" style="color:green">Наверх</a></p>
+    <p>© 2023 Компания, Inc. · <a href="#" style="color:green">Конфиденциальность</a> · <a href="#" style="color:green">Условия</a></p>
+    <!-- Заказать звонок -->
+    <div type="button" class="callback-bt">
+      <div class="text-call">
+       <span style="font-family:Century gothic; color:#32CD32">Заказать<br>звонок</span>
+      </div>
+    </div>
+    <!-- Заказать звонок END -->
+    <!-- Кнопка наверх -->
+    <div class="btn-up btn-up_hide"></div>
+    <!-- Кнопка наверх END -->
+</footer>
+<!-- Футер END-->
